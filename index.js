70f9bca7e0bf0231c8896fde9b5212e8105e0765
michael <michgboxy@gmail.com>
2023-03-20 01:14:37 +0100

    application entry point

---
+++ index.js
@@ -0,0 +1,10 @@
+require('dotenv').config();
+const app = require('./server/server');
+const { client} = require('./api/v1/utils/redis');
+
+const { PORT } = process.env;
+
+app.listen(PORT, async () => {
+    await client.connect();
+    console.log(`server started on port ${PORT}`);
+});
