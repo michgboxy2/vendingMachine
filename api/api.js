8f779d6e693f334dd6453022c2315c23cea74b23
michael <michgboxy@gmail.com>
2023-03-22 22:54:11 +0100

    route file

--- api/api.js
+++ api/api.js
@@ -1,12 +1,16 @@
 const express = require("express"),
       api = express.Router(),
       userRouter = require('./v1/user/userRouter'),
-      productRouter = require('./v1/products/productRouter');
+      productRouter = require('./v1/products/productRouter'),
+      purchaseRouter = require('./v1/purchase/purchaseRouter'),
+      depositRouter = require('./v1/deposit/depositRouter');
 
 
-
+      api.use('/', purchaseRouter);
+      api.use('/', depositRouter);
       api.use('/user', userRouter);
       api.use('/product', productRouter);
+
 
 
       module.exports = api;
